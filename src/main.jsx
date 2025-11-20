35a68b139c13fe72a2aea809d34dcbdd8fe0a341
llee2 <llee2349@uw.edu>
2025-11-19 18:21:41 -0800

    Firebase Setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css'
 import App from './components/App.jsx'
 import { BrowserRouter } from 'react-router'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtACkvgx6ONxH73yQtCBvBd2ND6BEyh0o",
+  authDomain: "info340-web-app.firebaseapp.com",
+  projectId: "info340-web-app",
+  storageBucket: "info340-web-app.firebasestorage.app",
+  messagingSenderId: "1060014760686",
+  appId: "1:1060014760686:web:e31a226d38ee0538a4c9ff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BrowserRouter><App /></BrowserRouter>);
